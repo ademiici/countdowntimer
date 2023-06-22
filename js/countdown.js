669e65a8d11e187c73cb24a6294ed7dc75dc2f24
Ademi <afeez@Ademis-MacBook-Pro.local>
2023-06-22 18:08:07 +0100

    added a background

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-let countDownDate = new Date("August 11, 2023 13:00:00").getTime();
+let countDownDate = new Date("August 11, 3023 13:00:00").getTime();
 
 // Update the count down every 1 second
 let x = setInterval(function() {
